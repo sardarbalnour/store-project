4315a01047e89f4eeb33f106fc5b7e320c2c4adc
Sardar balnour <balnour.sardar@gmail.com>
2026-07-10 11:59:14 +0330

    search box

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
+
 import { useProducts } from "../context/ProductsContext";
 
 import Card from "../components/Card";
@@ -7,18 +10,36 @@ import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
   const products = useProducts();
-  console.log(products);
+
+  const [search, setSearch] = useState("");
+
+  const serachHandler = () => {
+    console.log(search);
+  };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={serachHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>Sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>Sidebar</div>
+      </div>
+    </>
   );
 }
 
