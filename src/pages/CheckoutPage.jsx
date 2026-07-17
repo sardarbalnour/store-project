5cda0ace717eb0ef92da9caf8b07f0d01642237e
Sardar balnour <balnour.sardar@gmail.com>
2026-07-17 23:41:19 +0330

    checkoutPage

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,8 +1,24 @@
+import BascketCard from "../components/BascketCard";
+import { useCart } from "../context/CartContext";
 
 function CheckoutPage() {
+  const [state, dispatch] = useCart();
+
+  const clickHandler = (type, payload) => dispatch({ type, payload });
+
   return (
-    <div>CheckoutPage</div>
-  )
+    <div>
+      <div>
+        {state.selectedItems.map((product) => (
+          <BascketCard
+            key={product.id}
+            data={product}
+            clickHandler={clickHandler}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default CheckoutPage
+export default CheckoutPage;
