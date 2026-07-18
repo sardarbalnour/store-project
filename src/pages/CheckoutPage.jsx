8bb5d802fd5038d172a90face862aa956a9a75c3
Sardar balnour <balnour.sardar@gmail.com>
2026-07-18 17:07:56 +0330

    cart sidebar

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,14 +1,23 @@
 import BascketCard from "../components/BascketCard";
+import BascketSidebar from "../components/BascketSidebar";
+
 import { useCart } from "../context/CartContext";
+
+import styles from "./CheckoutPage.module.css";
 
 function CheckoutPage() {
   const [state, dispatch] = useCart();
 
   const clickHandler = (type, payload) => dispatch({ type, payload });
 
+  if (!state.itemsCounter) {
+    return <div className={styles.container}>empty</div>;
+  }
+
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <BascketSidebar state={state} clickHandler={clickHandler} />
+      <div className={styles.products}>
         {state.selectedItems.map((product) => (
           <BascketCard
             key={product.id}
